d983b0011965e8220b67a827a5f59d162aa2437b
Hendrikto <hendrik.to@gmail.com>
2017-04-16 17:17:41 +0200

    Answer type A DNS requests from the zone files.

--- dns/server.py
+++ dns/server.py
@@ -5,26 +5,52 @@
 This module provides a recursive DNS server. You will have to implement this
 server using the algorithm described in section 4.3.2 of RFC 1034.
 """
-
-
+import socket
 from threading import Thread
+
+from dns.message import Message, Header
+from dns.zone import Catalog
 
 
 class RequestHandler(Thread):
     """A handler for requests to the DNS server"""
 
-    def __init__(self):
+    def __init__(self, sock, data, address):
         """Initialize the handler thread"""
         super().__init__()
         self.daemon = True
+        self.sock = sock
+        self.data = data
+        self.address = address
+
+    def lookup_zone(self):
+        """Look for a record in the zone files."""
+        for i in range(len(self.domain.labels) + 1):
+            zone = ".".join(self.domain.labels[i:]) + "."
+            if zone in Server.catalog.zones:
+                if zone in Server.catalog.zones:
+                    records = Server.catalog.zones[zone].records
+                    return records[
+                        str(self.domain)[:str(self.domain).rfind(zone)]
+                    ]
+                else:
+                    return
 
     def run(self):
         """ Run the handler thread"""
-        pass
+        print("Thread started for:", self.data, self.address)
+        message = Message.from_bytes(self.data)
+        self.domain = message.questions[0].qname
+        records = self.lookup_zone()
+        header = Header(message.header.ident, 0, 0, 1, 0, 0)
+        response = Message(header, answers=records)
+        self.sock.sendto(response.to_bytes(), self.address)
 
 
 class Server:
     """A recursive DNS server"""
+
+    catalog = Catalog()
 
     def __init__(self, port, caching, ttl):
         """Initialize the server
@@ -41,9 +67,13 @@ class Server:
 
     def serve(self):
         """Start serving requests"""
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("127.0.0.1", self.port))
         while not self.done:
-            pass
+            data, address = self.sock.recvfrom(1024)
+            RequestHandler(self.sock, data, address).start()
 
     def shutdown(self):
         """Shut the server down"""
         self.done = True
+        self.sock.close()
