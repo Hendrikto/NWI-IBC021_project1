Hendrikto/NWI-IBC021_project1

commit c07adc199cafc171cb7178e61ec9d82b737c447d

Return CNAME records from the cache as well.

dns/resolver.py
@@ -38,9 +38,14 @@ class Resolver:
 
     def query_recursive(self, sock, hostname, ip):
         if self.cache is not None:
+            answer = []
             record = self.cache.lookup(Name(hostname), Type.A, Class.IN)
+            alias = self.cache.lookup(Name(hostname), Type.CNAME, Class.IN)
             if record is not None:
-                return [record]
+                answer.append(record)
+            if alias is not None:
+                answer.append(alias)
+            return answer
 
         response = Resolver.send_query(sock, hostname, ip)
         if (

dns_tests.py
@@ -11,7 +11,7 @@ from dns.cache import RecordCache
 from dns.classes import Class
 from dns.name import Name
 from dns.resolver import Resolver
-from dns.resource import ResourceRecord, ARecordData
+from dns.resource import ResourceRecord, ARecordData, CNAMERecordData
 from dns.types import Type
 
 PORT = 5001
@@ -106,9 +106,16 @@ class TestResolverCache(TestCase):
             ttl=60,
             rdata=ARecordData("1.0.0.1"),
         ))
+        cache.add_record(ResourceRecord(
+            name=Name("bonobo.putin"),
+            type_=Type.CNAME,
+            class_=Class.IN,
+            ttl=60,
+            rdata=CNAMERecordData(Name("putin.bonobo")),
+        ))
         self.assertEqual(
             resolver.gethostbyname("bonobo.putin"),
-            ("bonobo.putin", [], ["1.0.0.1"])
+            ("bonobo.putin", ["putin.bonobo."], ["1.0.0.1"])
         )
 
     def test_expired_cache_entry(self):
@@ -120,6 +127,13 @@ class TestResolverCache(TestCase):
             class_=Class.IN,
             ttl=0,
             rdata=ARecordData("1.0.0.2"),
+        ))
+        cache.add_record(ResourceRecord(
+            name=Name("hw.gumpe"),
+            type_=Type.CNAME,
+            class_=Class.IN,
+            ttl=0,
+            rdata=CNAMERecordData(Name("gumpe.hw")),
         ))
         self.assertEqual(
             resolver.gethostbyname("hw.gumpe"),

commit f38f16f48556d222e39424fbe9c91669446dbd3a

Fix resolver bug introduced in c07adc199c.
* Only return entries from the cache if any were found. Otherwise use the
  resolver.

dns/resolver.py
@@ -45,7 +45,8 @@ class Resolver:
                 answer.append(record)
             if alias is not None:
                 answer.append(alias)
-            return answer
+            if len(answer):
+                return answer
 
         response = Resolver.send_query(sock, hostname, ip)
         if (